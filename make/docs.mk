acba478e27a1d20f1cf8a74e6cbd21f8ba4163a0
Francesco Ilario <filario@redhat.com>
2022-11-10 12:55:26 +0100

    Update Antora site generation for local dev
    Signed-off-by: Francesco Ilario <filario@redhat.com>

--- make/docs.mk
+++ make/docs.mk
@@ -8,4 +8,7 @@ SITE_SOURCE ?= local
 .PHONY: site
 ## render site
 site:
+ifeq ($(SITE_SOURCE),local)
+	SITE_URL=${PWD}/out/site envsubst < antora-playbook.local.yaml.tmpl > antora-playbook.local.yaml
+endif
 	$(CONTAINER_RUNTIME) run -u $(shell id -u) -e CI=true -e HOME=/antora -v ${PWD}:/antora:Z --rm -t antora/antora:3.0.1 antora-playbook.$(SITE_SOURCE).yaml
